249449f085448a2cd3d709ec81356cabf6dcb385
RumbleRum <dnjswls0339@naver.com>
2024-10-30 11:57:28 +0900

    c1 업로드

---
+++ C1/script/index.js
@@ -0,0 +1,40 @@
+
+//nav
+$('li.main-menu').mouseenter(function () {
+    $(this).find('ul.sub-menu').stop().slideDown()
+})
+
+$('li.main-menu').mouseleave(function () {
+    $(this).find('ul.sub-menu').stop().slideUp()
+})
+
+//slide
+
+$('.slide').eq(0).siblings().hide()
+let idx = 0
+setInterval(function () {
+    idx++
+    if (idx === 3) {idx = 0}
+    $('.slide').fadeOut()
+    $('.slide').eq(idx).fadeIn()
+
+}, 3000)
+
+//tab
+$('.tab-menu li').click(function(){
+    $('.tab-menu li').removeClass('on')
+    $(this).addClass('on')
+
+    let idx = $(this).index()
+    $('.tab').hide()
+    $('.tab').eq(idx).show()
+})
+
+//popup
+$('.modal').click(function(){
+    $('.popup').fadeIn()
+})
+
+$('a.close').click(function(){
+    $('.popup').fadeOut()
+})
