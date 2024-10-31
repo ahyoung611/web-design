223c67c27dbf0bedfe3b85a698f78f5980314d85
RumbleRum <dnjswls0339@naver.com>
2024-10-31 12:53:36 +0900

    c2 업로드

---
+++ C3/script/index.js
@@ -0,0 +1,24 @@
+//nav
+$('li.main-menu').mouseenter(function(){
+    $(this).find('ul.sub-menu').stop().fadeIn()
+})
+$('li.main-menu').mouseleave(function(){
+    $(this).find('ul.sub-menu').stop().fadeOut()
+})
+
+//slide
+setInterval(function(){
+$('.slide-wrap').animate({'left':'-100%'},function(){
+    $('.slide:first').appendTo('.slide-wrap')
+    $('.slide-wrap').css({'left':'0'})
+})
+},3000)
+
+//popup
+$('.modal').click(function(){
+    $('.popup').fadeIn()
+})
+
+$('a.close').click(function(){
+    $('.popup').fadeOut()
+})
