9df6b85b4d925c5b71c4b6b8f639a9723fabd151
RumbleRum <dnjswls0339@naver.com>
2024-11-05 16:23:39 +0900

    e1

---
+++ E1/script/index.js
@@ -0,0 +1,25 @@
+//nav
+$('li.main-menu').mouseenter(function(){
+    $(this).find('ul.sub-menu').stop().slideDown()
+})
+
+$('li.main-menu').mouseleave(function(){
+    $(this).find('ul.sub-menu').stop().slideUp()
+})
+
+// slide
+setInterval(function(){
+$('.slide-wrap').animate({'left':'-100%'},function(){
+    $('.slide:first').appendTo('.slide-wrap')
+    $('.slide-wrap').css({'left':'0'})
+})
+},3000)
+
+// popup
+$('li.modal').click(function(){
+    $('.modal-box').fadeIn()
+})
+
+$('a.close').click(function(){
+    $('.modal-box').fadeOut()
+})
